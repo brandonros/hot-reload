4da5203d037caf9549a790fdd3099743e25c9173
Brandon Ros <brandonros1@gmail.com>
2016-02-13 22:59:32 -0500

    one method

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var lib = require('./lib');
 
+function reload(module) {
+	delete require.cache[require.resolve(module)];
+	return require(module);
+}
+
 setInterval(function () {
 	console.log(lib.version);
 }, 1000);
+
+process.on('SIGUSR2', function () {
+	lib = reload('./lib');
+});
